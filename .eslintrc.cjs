13672b78d793836b793cc00f677e6e19a45e8ba3
devRochaa <danielrocha.etec@gmail.com>
2025-07-01 09:11:39 -0300

    Type anotation

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  root: true, // Garante que esse é o config principal
+  env: {
+    browser: true,
+    es2020: true,
+    node: true
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
+    // 'plugin:@typescript-eslint/recommended-requiring-type-checking'
+  ],
+  rules: {
+    // regras personalizadas
+  },
+  ignorePatterns: [
+    'node_modules/',
+    'dist/',
+    '.eslintrc.cjs'
+  ]
+};
---
+++ src/A0001-type-annotation/index.ts
@@ -0,0 +1,13 @@
+/* eslint-disable */
+let nome: string = 'Daniel';
+
+let simbolo: symbol = Symbol('qualquer-symbol');
+let big: bigint = 10n;
+
+
+//array
+let arrayDeNumeros: Array<number> = [1,2, 3];
+let arrayDeStrings: Array<string> = ["1", '2', 'tres'];
+//ou
+let arrayDeNumeros2: number[] = [1,2, 3];
+let arrayDeStrings2: string[] = ["1", '2', 'tres'];
